5f797a1790e214945802c2337525da0c8f9b5afb
oibioib <pavelbodal@gmail.com>
2023-05-21 00:24:01 +0200

    feat: add assets

---
+++ src/assets/index.ts
@@ -0,0 +1,11 @@
+export { ReactComponent as ErrorImage } from './404.svg';
+export { ReactComponent as EmptyImage } from './empty.svg';
+export { ReactComponent as ChevronDownIcon } from './icon/chevron_down_icon.svg';
+export { ReactComponent as CrossIcon } from './icon/cross_icon.svg';
+export { ReactComponent as DownIcon } from './icon/down_icon.svg';
+export { ReactComponent as PointIcon } from './icon/point_icon.svg';
+export { ReactComponent as SearchIcon } from './icon/search_icon.svg';
+export { ReactComponent as StarIcon } from './icon/star_icon.svg';
+export { ReactComponent as UpIcon } from './icon/up_icon.svg';
+export { ReactComponent as LogoSvg } from './logo.svg';
+export { ReactComponent as Point } from './point.svg';
