1cd6e67a211795ce6d911045a25a52fc1dc5300d
oibioib <pavelbodal@gmail.com>
2023-05-21 00:15:37 +0200

    feat: add theme and query provider to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import { Test } from '@components';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import { MantineProvider } from '@mantine/core';
+import { routes } from '@routes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { theme } from '@theme';
+import '@theme/fonts';
+
+const router = createBrowserRouter(routes);
+const queryClient = new QueryClient();
 
 const App = () => {
-  return <Test />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
+        <RouterProvider router={router} />
+      </MantineProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
